4aa4291597714eda5b511483c972344ac7354a89
Joe Lee-Moyet <github@yjo.me>
2020-10-26 08:28:10 +0000

    [not working] OTA update

---
+++ lib/SNU/examples/Usage/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+#define SECRET_OTA_URL "http://downloads.arduino.cc/misc/WiFi1010_blinkRBG.bin"
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,72 @@
-#include <Arduino.h>
-#include <WS2812FX.h>
-#include <Adafruit_NeoPixel.h>
+#include <SNU.h>
+_SFU_H_INCLUDED // suppress unused header inspection
+#include <WiFiNINA.h>
 
-#define LED_COUNT 120
-#define LED_PIN 13
-#define MAX_BRIGHTNESS 255
+#include "arduino_secrets.h"
 
-WS2812FX ledFx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+const char wifiSsid[] = WIFI_SSID;
+const char wifiPassword[] = WIFI_PASS;
+const char url[] = SECRET_OTA_URL;
 
-void changeLedFxMode(int newLedFxMode) {
-  newLedFxMode = constrain(newLedFxMode, 0, FX_MODE_CUSTOM);
-  ledFx.setMode(newLedFxMode);
-  Serial.print(newLedFxMode);
-  Serial.print(": ");
-  Serial.println(ledFx.getModeName(newLedFxMode));
-}
+int status = WL_IDLE_STATUS;
 
-void changeLedFxSpeed(uint16_t newSpeed) {
-  newSpeed = constrain(newSpeed, SPEED_MIN, SPEED_MAX);
-  ledFx.setSpeed(newSpeed);
-  Serial.print("Spd: ");
-  Serial.println(newSpeed);
-}
+String message;
 
 void setup() {
   Serial.begin(256000);
-  Serial.println("\n>>");
-  ledFx.init();
-  ledFx.setColor(WHITE);
-  ledFx.setBrightness(MAX_BRIGHTNESS);
-  changeLedFxSpeed(256);
-  changeLedFxMode(FX_MODE_RAINBOW_CYCLE);
-  ledFx.start();
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(wifiSsid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(wifiSsid, wifiPassword);
+  }
+
+  // wait a bit
+  delay(1000);
+
+  Serial.print("WiFi fw version: ");
+  Serial.println(WiFi.firmwareVersion());
+  Serial.print("MAC: ");
+  auto macAddr = WiFiDrv::getMacAddress();
+  for (int i = 0; i < WL_MAC_ADDR_LENGTH; ++i) {
+    Serial.print(macAddr[i], HEX);
+  }
+  Serial.println();
+
+  message += "Sketch compile date and time: ";
+  message += __DATE__;
+  message += " ";
+  message += __TIME__;
+
+  // print out the sketch compile date and time on the serial port
+  Serial.println(message);
+//  Serial1.println(message);
+
+  Serial.println("Type \"download\" in the Serial Monitor to start downloading the update");
 }
 
 void loop() {
-  const uint32_t now_us = micros();
-  switch (char c = Serial.read()) {
-    case '8':
-      changeLedFxMode(ledFx.getMode() + 1);
-      break;
-    case '2':
-      changeLedFxMode(ledFx.getMode() - 1);
-      break;
-    case '7':
-      changeLedFxSpeed(ledFx.getSpeed() << 1);
-      break;
-    case '1':
-      changeLedFxSpeed(ledFx.getSpeed() >> 1);
-      break;
+  String command = Serial.readStringUntil('\n');
+
+  if (command.indexOf("download") == 0) {
+
+    Serial.println("Downloading update file");
+    //    WiFiStorage.download(url, "UPDATE.BIN");
+
+    const char *filename = "UPDATE.BIN";
+    int8_t dlRetVal = WiFiDrv::downloadFile(url, strlen(url), filename, strlen(filename));
+    Serial.println("dlretval: " + String(dlRetVal));
+
+    WiFiStorageFile update = WiFiStorage.open("/fs/UPDATE.BIN");
+    if (update.available()) {
+      Serial.println("Download complete, please restart or type \"restart\" to apply the update");
+      Serial.println("Filesize: " + String(update.available()));
+    } else {
+      Serial.println("Download failed, please retry :(");
+    }
+  } else if (command.indexOf("restart") >= 0) {
+    NVIC_SystemReset();
   }
-  ledFx.service();
 }
