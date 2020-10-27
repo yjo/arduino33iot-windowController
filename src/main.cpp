d07e8bc03dadf014917c44065e3a77c60ee21086
Joe Lee-Moyet <github@yjo.me>
2020-10-27 18:55:43 +0000

    Add RGB controller to motor code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,49 @@
 #include <Arduino.h>
+#include <WS2812FX.h>
+#include <Adafruit_NeoPixel.h>
+
 #include "SlatsMotor.h"
-#include "util.h"
+
+#define LED_COUNT 120
+#define LED_PIN 13
+#define DEFAULT_BRIGHTNESS 50
 
 SlatsMotor slatsMotor;
+WS2812FX ledFx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+uint32_t colors[] = {RED, GREEN, BLUE};
+
+void changeLedFxMode(int newLedFxMode) {
+  newLedFxMode = constrain(newLedFxMode, 0, FX_MODE_CUSTOM);
+  ledFx.setMode(newLedFxMode);
+  Serial.print(newLedFxMode);
+  Serial.print(": ");
+  Serial.println(ledFx.getModeName(newLedFxMode));
+}
+
+void changeLedFxSpeed(int32_t newSpeed) {
+  newSpeed = constrain(newSpeed, SPEED_MIN, SPEED_MAX);
+  ledFx.setSpeed(newSpeed);
+  Serial.print("Spd: ");
+  Serial.println(newSpeed);
+}
+
+void changeLedFxBrightness(int16_t newBrightness) {
+  newBrightness = constrain(newBrightness, 1, 255);
+  ledFx.setBrightness(newBrightness);
+  Serial.print("Brightness: ");
+  Serial.println(newBrightness);
+}
 
 void setup() {
   Serial.begin(256000);
   Serial.println("\n>>");
+  ledFx.init();
+  ledFx.setColors(0, colors);
+  changeLedFxBrightness(DEFAULT_BRIGHTNESS);
+  changeLedFxSpeed(256);
+  changeLedFxMode(FX_MODE_RAINBOW_CYCLE);
+  ledFx.start();
 
   slatsMotor.init(A7);
 
@@ -14,5 +51,27 @@ void setup() {
 }
 
 void loop() {
+  const uint32_t now_us = micros();
+  switch (char c = Serial.read()) {
+    case '8':
+      changeLedFxMode(ledFx.getMode() + 1);
+      break;
+    case '2':
+      changeLedFxMode(ledFx.getMode() - 1);
+      break;
+    case '7':
+      changeLedFxSpeed(ledFx.getSpeed() << 1);
+      break;
+    case '1':
+      changeLedFxSpeed(ledFx.getSpeed() >> 1);
+      break;
+    case '9':
+      changeLedFxBrightness(ledFx.getBrightness() + 1);
+      break;
+    case '3':
+      changeLedFxBrightness(ledFx.getBrightness() - 1);
+      break;
+  }
+  ledFx.service();
   slatsMotor.service();
 }
